8b6883a9fe8565d618a44fc71dc46d848d8f5e1c
Qingyun <79644233+Li-Qingyun@users.noreply.github.com>
2025-08-10 12:44:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ set -x
 export TORCH_CUDA_ARCH_LIST=$(python -c "import torch; print(f'{torch.cuda.get_device_capability()[0]}.{torch.cuda.get_device_capability()[1]}')")
 
 pip install -r requirements/optional.txt
-pip install -e . -v
+MMCV_WITH_OPS=1 FORCE_CUDA=1 TORCH_CUDA_ARCH_LIST=$TORCH_CUDA_ARCH_LIST pip install -e . -v
